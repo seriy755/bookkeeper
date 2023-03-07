b92955dd8e8c9e8ff23f380169d3809317e12db9
seriy755 <serg.la4in@yandex.ru>
2023-03-07 15:02:04 +0300

    Update tests for budget model

--- bookkeeper/models/budget.py
+++ bookkeeper/models/budget.py
@@ -5,6 +5,7 @@
 from dataclasses import dataclass
 from datetime import datetime, timedelta
 
+from bookkeeper.models.expense import Expense
 from ..repository.abstract_repository import AbstractRepository
 
 
@@ -19,50 +20,48 @@ class Budget:
     pk - id записи в базе данных
     """
     amount: int
-    category: int | None
-    restrict: int
     period: str
+    category: int | None = None
+    restrict: int | None = None
     pk: int = 0
-    
-    
-    def get_amounts(repo: AbstractRepository['Expense']) -> list[int]:
-        """
-        Посчитать сумму расходов за день, неделю, месяц
-        """ 
+
+    @classmethod
+    def get_amounts(cls,
+                    repo: AbstractRepository['Expense']) -> list[int]:
+        "Функция для подсчёта суммы расходов за день, неделю, месяц"
         day_amount = 0
         date = datetime.now()
         day_expenses = repo.get_all(where={"expense_date":
                                            date.strftime('%Y-%m-%d')})
-        day_amount += sum([exp.amount for exp in day_expenses])
-        
+        day_amount += sum(exp.amount for exp in day_expenses)
+
         week_amount = 0
         for i in range(date.weekday() + 1):
             weekday = date - timedelta(days=i)
             weekday_expenses = repo.get_all(where={"expense_date":
                                                    weekday.strftime('%Y-%m-%d')})
-            week_amount += sum([exp.amount for exp in weekday_expenses])
-        
-        month_amount = 0 
+            week_amount += sum(exp.amount for exp in weekday_expenses)
+
+        month_amount = 0
         for i in range(date.day):
             monthday = date - timedelta(days=i)
             month_expenses = repo.get_all(where={"expense_date":
                                                  monthday.strftime('%Y-%m-%d')})
-            month_amount += sum([exp.amount for exp in month_expenses])
+            month_amount += sum(exp.amount for exp in month_expenses)
         return [day_amount, week_amount, month_amount]
-           
-    
+
     @classmethod
-    def create_from_list(
+    def create_from_restricts(
             cls,
-            restricts: list[tuple[int | None, int]],
-            repo: AbstractRepository['Expense']):
+            restricts: list[tuple[int | None, int | None]],
+            repo: AbstractRepository['Expense']) -> list['Budget']:
+        "Функция для создания списка бюджета из списка ограничений"
         periods = ('день', 'неделя', 'месяц')
         amounts = cls.get_amounts(repo)
         created: list[Budget] = []
         for values, period, amount in zip(restricts, periods, amounts):
             cat = values[0]
             restrict = values[1]
-            budg = cls(amount, cat, restrict, period)
-            created.append(budg)
+            budget = cls(amount, period, cat, restrict)
+            created.append(budget)
         return created
-            
--- tests/test_models/test_budget.py
+++ tests/test_models/test_budget.py
@@ -1,9 +1,13 @@
+"""
+Тесты для модели бюджета
+"""
 from datetime import datetime
 
 import pytest
 
-from bookkeeper.repository.memory_repository import MemoryRepository
 from bookkeeper.models.budget import Budget
+from bookkeeper.repository.memory_repository import MemoryRepository
+from bookkeeper.models.expense import Expense
 
 
 @pytest.fixture
@@ -11,20 +15,59 @@ def repo():
     return MemoryRepository()
 
 
-def test_create_with_full_args_list():
-    b = Budget(amount=100, category=1, period=7, pk=1)
+def test_create_object():
+    b = Budget(amount=100, period='день', category=1, restrict=1000, pk=1)
     assert b.amount == 100
     assert b.category == 1
-    assert b.period == 7
-
-
-def test_create_brief():
-    b = Budget(100, 1)
+    assert b.restrict == 1000
+    assert b.period == 'день'
+    assert b.pk == 1
+    
+    b = Budget(100, 'месяц')
     assert b.amount == 100
-    assert b.category == 1
+    assert b.category == None
+    assert b.restrict == None
+    assert b.period == 'месяц'
+    assert b.pk == 0
+    
+
+def test_reassign():
+    """
+    budget should not be frozen
+    """
+    b = Budget(100, 'день')
+    b.period = 'месяц'
+    b.pk = 1
+    assert b.period == 'месяц'
+    assert b.pk == 1
 
 
-def test_can_add_to_repo(repo):
-    b = Budget(100, 1)
-    pk = repo.add(b)
-    assert b.pk == pk
+def test_eq():
+    """
+    budget should implement __eq__ method
+    """
+    b1 = Budget(amount=100, period='день', category=1)
+    b2 = Budget(amount=100, period='день', category=1)
+    assert b1 == b2   
+
+
+def test_get_amounts(repo):
+    e1 = Expense(100, 1)
+    e2 = e = Expense(100, 1)
+    repo.add(e1)
+    repo.add(e2)
+    amounts = [200, 200, 200]
+    assert Budget.get_amounts(repo) == amounts
+    
+
+def test_create_from_restricts(repo):
+    e1 = Expense(100, 1)
+    e2 = e = Expense(100, 1)
+    repo.add(e1)
+    repo.add(e2)
+    restricts = [(1, 1000), (None, 7000), (None, None)]
+    b1 = Budget(200, 'день', 1, 1000)
+    b2 = Budget(200, 'неделя', None, 7000)
+    b3 = Budget(200, 'месяц', None, None)
+    budgets = [b1, b2, b3]
+    assert Budget.create_from_restricts(restricts, repo) == budgets
